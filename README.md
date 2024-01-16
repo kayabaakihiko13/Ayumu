c99a68591c31acde70fb414b45d83081e1f170b8
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-01-16 14:44:33 +0700

    feat: setup for prepratation modul
    Signed-off-by: kayabaakihiko13 <iqbalramad75@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Ayumi
-Data Clustering With Anime Picture
+# Ayumu
+![image](Doc\image\XGNQlmUw.png)
+
+**Image Segmentation Anime Picture**
+
+Detail
+- Image Segmentation is commonly used technique in digital image preprocessing and analysis
+to partition an image into multiple parts or regions,often based on the characteristics of the pixels in the image.Image segementation could involve separating foreground from background, or clustering regions of pixels based on similarities in color or shape.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("requirements.txt") as file:
+    requitments = file.read().splitlines()
+    
+setup(
+    name="Ayumu",
+    version="0.0.1",
+    description="Segmentation Image",
+    install_requires=requitments,
+    python_requires=">=3.10",
+    license="MIT License",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ]
+)
